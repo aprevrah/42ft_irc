efa83971302c1d184fefbc3e1e1d7febd787c483
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-07-17 17:56:35 +0200

    formatting

--- Channel.hpp
+++ Channel.hpp
@@ -1,35 +1,38 @@
 #pragma once
-#include <string>
 #include <map>
 #include <stdexcept>
+#include <string>
+
 #include "IRCException.hpp"
 #include "Numerics.hpp"
 
 class Client;
 
 class Channel {
-    public:
-        Channel();
-        Channel(const std::string &name);
-        ~Channel();
-        
-        // Channel operations
-        bool is_full() const;
-        size_t nbr_of_clients() const;
-        void join_client(Client* client, bool is_operator = false);
-        void leave_client(Client* client);
-        bool is_client_in_channel(Client* client) const;
-        bool is_client_operator(Client* client) const;
-        const std::string& get_name() const;
-        void set_topic(const std::string& new_topic);
-        const std::string& get_topic() const;
-    private:
-        std::map<Client*, bool> clients;
-        std::string name;
-        std::string topic;
-        //modes
-        bool invite_only;
-        bool topic_needs_op;
-        std::string passwd;
-        unsigned int user_limit; //0 is no limit
+   private:
+    // I'm guessing the bool value stands for wether the client is an operator?
+    std::map<Client*, bool> clients;
+    std::string             name;
+    std::string             topic;
+    // modes
+    bool         invite_only;
+    bool         topic_needs_op;
+    std::string  passwd;
+    unsigned int user_limit;  // 0 is no limit
+
+   public:
+    Channel();
+    Channel(const std::string& name);
+    ~Channel();
+
+    // Channel operations
+    bool               is_full() const;
+    size_t             nbr_of_clients() const;
+    void               join_client(Client* client, bool is_operator = false);
+    void               leave_client(Client* client);
+    bool               is_client_in_channel(Client* client) const;
+    bool               is_client_operator(Client* client) const;
+    const std::string& get_name() const;
+    void               set_topic(const std::string& new_topic);
+    const std::string& get_topic() const;
 };
