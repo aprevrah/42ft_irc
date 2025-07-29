c1584a0cbe5158db0d9f1f03a2a2a29ee896e538
Aprevrha <aprevrha@gmail.com>
2025-07-30 00:38:02 +0200

    irc format ping

--- src/commands/cmd_ping.cpp
+++ src/commands/cmd_ping.cpp
@@ -4,10 +4,10 @@ t_command_status Command::cmd_ping(Server* server) {
     (void)server;
     if (parameters.size() > 0) {
         std::string& token = parameters.back();
-        client.send_response("PONG " SERVER_NAME " " + token);  // TODO: server name var?
+        client.send_response("PONG " SERVER_NAME " :" + token); // TODO: server name var?
         return CMD_SUCCESS;
     } else {
-        client.send_response(to_string(ERR_NEEDMOREPARAMS));
+        client.send_numeric_response(ERR_NEEDMOREPARAMS, "PING", "Not enough parameters");
         return CMD_FAILURE;
     }
 }
